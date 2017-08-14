a54b719a178157d6f5671f764770ff632af7283a
Tim Mecklem <timothy@mecklem.com>
2017-08-13 20:29:41 -0400

    Rename to infinity_system_rpi0

--- nerves.exs
+++ nerves.exs
@@ -5,14 +5,14 @@ version =
   |> File.read!
   |> String.trim
 
-pkg = :nerves_system_rpi0
+pkg = :infinity_system_rpi0
 
 config pkg, :nerves_env,
   type: :system,
   version: version,
   compiler: :nerves_package,
   artifact_url: [
-    "https://github.com/nerves-project/#{pkg}/releases/download/v#{version}/#{pkg}-v#{version}.tar.gz",
+    "https://github.com/infinity-aps/#{pkg}/releases/download/v#{version}/#{pkg}-v#{version}.tar.gz",
   ],
   platform: Nerves.System.BR,
   platform_config: [
